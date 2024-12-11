f2fa83871222d3f1084da642ab142b7be87d92a9
Tyler Boutwell <tylerboutwell1@gmail.com>
2024-12-10 18:12:59 -0600

    set up flask start, and app.route decorator

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from flask import Flask, request, jsonify
 
-
 api_key = "YOUR_API_KEY"
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Home"
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
