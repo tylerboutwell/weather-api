a1f2a52871ba5e5096bc6d066ff96a36f2d5e063
Tyler Boutwell <tylerboutwell1@gmail.com>
2024-12-10 18:31:00 -0600

    set up json return

--- main.py
+++ main.py
@@ -4,9 +4,18 @@ api_key = "YOUR_API_KEY"
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Home"
+@app.route("/get-user/<user_id>")
+def get_user(user_id):
+    user_data = {
+        "user_id": user_id,
+        "name" : "John Smith",
+        "email" : "johnsmith@example.com"
+    }
+    extra = request.args.get("extra")
+    if extra:
+        user_data["extra"] = extra
+
+    return jsonify(user_data), 200
 
 if __name__ == "__main__":
     app.run(debug=True)
