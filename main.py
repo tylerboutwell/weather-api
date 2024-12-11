89917d7114cf3edb362f7c88ce2f3539fe04c7c4
Tyler Boutwell <tylerboutwell1@gmail.com>
2024-12-11 06:58:31 -0600

    switch to fastapi

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
-from flask import Flask, request, jsonify
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional, List
+from uuid import uuid4, UUID
 
 api_key = "YOUR_API_KEY"
+app = FastAPI()
 
-app = Flask(__name__)
+class Task(BaseModel):
+    id: Optional[UUID] = None
+    title: str
+    description: Optional[str] = None
+    completed: bool = False
 
-@app.route("/get-user/<user_id>")
-def get_user(user_id):
-    user_data = {
-        "user_id": user_id,
-        "name" : "John Smith",
-        "email" : "johnsmith@example.com"
-    }
-    extra = request.args.get("extra")
-    if extra:
-        user_data["extra"] = extra
+tasks = []
 
-    return jsonify(user_data), 200
+@app.post("/tasks/", response_model=Task)
+def create_task(task: Task):
+    task.id = uuid4()
+    tasks.append(task)
+    return task
+
+@app.get("/tasks/", response_model = List[Task])
+def get_tasks():
+    return tasks
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import  uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
 
