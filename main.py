80f9532892ec4d929b7a8413bd8fc4a31447fd30
Tyler Boutwell <tylerboutwell1@gmail.com>
2024-12-22 18:40:54 -0600

    remove unneccesary functions

--- main.py
+++ main.py
@@ -9,54 +9,14 @@ from dotenv import load_dotenv
 load_dotenv()
 api_key = os.getenv("api_key")
 app = FastAPI()
-
-class Forecast(BaseModel):
-    id: UUID | None = None
-    title: str
-    description: str | None = None
-
-forecasts = []
-
-@app.post("/forecasts/", response_model=Forecast)
-def create_forecast(forecast: Forecast):
-    forecast.id = uuid4()
-    forecasts.append(forecast)
-    return forecast
-
-@app.get("/forecasts/", response_model = list[Forecast])
-def get_forecasts():
-    return forecasts
-
-@app.get("/forecasts/{forecast_id}", response_model=Forecast)
-def get_forecast(forecast_id: UUID):
-    for forecast in forecasts:
-        if forecast.id == forecast_id:
-            return forecast
-    raise HTTPException(status_code=404, detail="Forecast not found")
-
-@app.get("/forecasts/current/")
-def get_current_forecast(lat: int, lon: int):
+@app.get("/current/")
+def get_current_weather(lat: int, lon: int):
     if not api_key:
         raise HTTPException(status_code=404, detail=f"No API key provided. Showing {api_key}")
     curr = requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}&units=imperial")
     if curr.status_code != 200:
         raise HTTPException(status_code=curr.status_code, detail=f"Forecast not found, api = {api_key}")
     return curr.json()
-
-@app.put("/forecasts/{forecast_id}", response_model=Forecast)
-def update_forecast(forecast_id: UUID, forecast_update: Forecast):
-    for ind, item in enumerate(forecasts):
-        if item.id == forecast_id:
-            forecasts[ind] = item.copy(update=forecast_update.model_dump(exclude_unset=True))
-            return forecasts[ind]
-    raise HTTPException(status_code=404, detail="Forecast not found")
-
-@app.delete("/forecasts/{forecast_id}", response_model=Forecast)
-def delete_forecast(forecast_id: UUID):
-    for ind, forecast in enumerate(forecasts):
-        if forecast.id == forecast_id:
-            return forecasts.pop(ind)
-    raise HTTPException(status_code=404, detail="Forecast not found")
 
 if __name__ == "__main__":
     import uvicorn
