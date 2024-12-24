56b4ac6af44b1ae1ad67593ac406535fc6aa6d9b
Tyler Boutwell <tylerboutwell1@gmail.com>
2024-12-23 19:50:20 -0600

    implement caching with redis

--- main.py
+++ main.py
@@ -5,18 +5,30 @@ from pydantic import BaseModel
 from uuid import uuid4, UUID
 import requests
 from dotenv import load_dotenv
+import redis
+import json
+from contextlib import asynccontextmanager
 
 load_dotenv()
 api_key = os.getenv("api_key")
+rd = redis.Redis(host='localhost', port=6379, db=0)
 app = FastAPI()
+
 @app.get("/current/")
-def get_current_weather(lat: int, lon: int):
+def get_current_weather(city: str):
     if not api_key:
         raise HTTPException(status_code=404, detail=f"No API key provided. Showing {api_key}")
-    curr = requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}&units=imperial")
-    if curr.status_code != 200:
-        raise HTTPException(status_code=curr.status_code, detail=f"Forecast not found, api = {api_key}")
-    return curr.json()
+    cache = rd.get(city)
+    if cache:
+        print("Cache hit")
+        return json.loads(cache)
+    else:
+        print("Cache miss")
+        curr = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}")
+        if curr.status_code != 200:
+            raise HTTPException(status_code=curr.status_code, detail=f"Forecast not found, api = {api_key}")
+        rd.set(city, curr.text)
+        return curr.json()
 
 if __name__ == "__main__":
     import uvicorn
